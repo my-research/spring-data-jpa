b92ae75cc7e0931881a1b748d3a6bbeaea9ee110
jangwonik <dhslrl321@gmail.com>
2024-05-06 19:14:58 +0900

    commit

---
+++ src/test/java/com/github/dhslrl321/step4/EntityTest.java
@@ -0,0 +1,27 @@
+package com.github.dhslrl321.step4;
+
+import com.github.dhslrl321.support.AbstractJpaTest;
+import jakarta.persistence.*;
+import org.junit.jupiter.api.Test;
+
+public class EntityTest extends AbstractJpaTest {
+    @Test
+    void name() {
+        entityManager.persist(new Foo());
+    }
+}
+
+@Entity
+@Table(name = "foos")
+class Foo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    String id = "foo";
+    @Enumerated(EnumType.STRING)
+    FooType type;
+}
+
+enum FooType {
+    NORMAL,
+    COMPLEX
+}
